aeb085d15a25e6e93b50cc9cac029758df9647d5
Md. Shahinur Islam <shahinalam6644@gmail.com>
2024-08-08 14:22:31 +0600

    logo add

--- app/src/main/java/com/nasheed/now/ButtonFragment.java
+++ app/src/main/java/com/nasheed/now/ButtonFragment.java
@@ -135,21 +135,3 @@ public class ButtonFragment extends Fragment {
                 .commit();
     }
 }
-
-//class Item {
-//    private int id;
-//    private String name;
-//
-//    public Item(int id, String name) {
-//        this.id = id;
-//        this.name = name;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//}
